0dab00806b72f2cdb8f5afd623aef1a407a6e9f4
Kvn11 <45984541+Kvn11@users.noreply.github.com>
2025-07-31 21:46:35 -0400

    add vulnerable function to  test_00.py

--- test_00.py
+++ test_00.py
@@ -1,7 +1,11 @@
 import os
 
+def bar():
+  user_input = input("Enter filename: ")
+  with open(user_input, 'r') as file:  # Vulnerable to directory traversal
+    content = file.read()
 
-def main():
+def foo():
   directory = input("Enter the directory to list: ")
   command = f"ls {directory}"
   os.system(command)
